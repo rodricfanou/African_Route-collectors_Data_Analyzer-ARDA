8eafde9f9cee63ccf04212757f9dcf30e8765095
rodricfanou <roderick.fanou@imdea.org>
2018-02-19 22:07:14 +0100

    Download and store details on ASNs allocated by AFRINIC

--- ComputationVM/Step2_Fill_RIRs_database/2_fill_historical_AFRINIC_v3_ASNsonly.py
+++ ComputationVM/Step2_Fill_RIRs_database/2_fill_historical_AFRINIC_v3_ASNsonly.py
@@ -2,7 +2,7 @@
 ## Author: Roderick Fanou
 ## Store ASNs allocated by AFRINIC to ISPs or any organization in its region
 ## Alert: this script may be run:
-## 1 - either frequently (at least once per trimester): In this case you can comment line 191 and use the cronjob
+## 1 - either frequently (at least once per trimester): In this case you can comment line 190 and use the cronjob
 ## 2 - or run the scripts updating the RIR data one after the other: in which case you can keep the scripts as they are.
 
 import MySQLdb, collections, sys, glob, math,  ast, os, time, random
@@ -42,7 +42,6 @@ time.sleep(value * random.randint(0,3))
 
 website = "ftp://ftp.afrinic.net/pub/stats/afrinic/"
 folder_download = "ftp.afrinic.net/pub/stats/afrinic/"
-
 print 'Download all the folders of allocation'
     
 command = """ wget -N -H -r --level=2 -k -p """ + website
