f85ff4ac899709cdd2fb6a76cf179aee31672596
Víctor Sánchez Agüero <vsaguero@gmail.com>
2018-04-03 20:11:50 +0200

    Add files via upload

---
+++ ComputationVM/Step3_DownloadPCHandRVdata/RV_daily_fun_v2.py
@@ -0,0 +1,207 @@
+#!/usr/bin/env python
+import MySQLdb
+import datetime
+import sys
+import time
+import urllib2, urllib, glob, re
+from _pybgpstream import BGPStream, BGPRecord, BGPElem
+import smtplib
+from smtplib import SMTPException
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
+import yagmail
+import os
+import DB_configuration
+
+def is_valid_v4(ip):
+    """Validates IPv4 addresses.
+        """
+    pattern = re.compile(r"""
+        ^
+        (?:
+        # Dotted variants:
+        (?:
+        # Decimal 1-255 (no leading 0's)
+        [3-9]\d?|2(?:5[0-5]|[0-4]?\d)?|1\d{0,2}
+        |
+        0x0*[0-9a-f]{1,2}  # Hexadecimal 0x0 - 0xFF (possible leading 0's)
+        |
+        0+[1-3]?[0-7]{0,2} # Octal 0 - 0377 (possible leading 0's)
+        )
+        (?:                  # Repeat 0-3 times, separated by a dot
+        \.
+        (?:
+        [3-9]\d?|2(?:5[0-5]|[0-4]?\d)?|1\d{0,2}
+        |
+        0x0*[0-9a-f]{1,2}
+        |
+        0+[1-3]?[0-7]{0,2}
+        )
+        ){0,3}
+        |
+        0x0*[0-9a-f]{1,8}    # Hexadecimal notation, 0x0 - 0xffffffff
+        |
+        0+[0-3]?[0-7]{0,10}  # Octal notation, 0 - 037777777777
+        |
+        # Decimal notation, 1-4294967295:
+        429496729[0-5]|42949672[0-8]\d|4294967[01]\d\d|429496[0-6]\d{3}|
+        42949[0-5]\d{4}|4294[0-8]\d{5}|429[0-3]\d{6}|42[0-8]\d{7}|
+        4[01]\d{8}|[1-3]\d{0,9}|[4-9]\d{0,8}
+        )
+        $
+        """, re.VERBOSE | re.IGNORECASE)
+    return pattern.match(ip) is not None
+
+
+def is_valid_v6(ip):
+    """Validates IPv6 addresses.
+        """
+    pattern = re.compile(r"""
+        ^
+        \s*                         # Leading whitespace
+        (?!.*::.*::)                # Only a single whildcard allowed
+        (?:(?!:)|:(?=:))            # Colon iff it would be part of a wildcard
+        (?:                         # Repeat 6 times:
+        [0-9a-f]{0,4}           #   A group of at most four hexadecimal digits
+        (?:(?<=::)|(?<!::):)    #   Colon unless preceeded by wildcard
+        ){6}                        #
+        (?:                         # Either
+        [0-9a-f]{0,4}           #   Another group
+        (?:(?<=::)|(?<!::):)    #   Colon unless preceeded by wildcard
+        [0-9a-f]{0,4}           #   Last group
+        (?: (?<=::)             #   Colon iff preceeded by exacly one colon
+        |  (?<!:)              #
+        |  (?<=:) (?<!::) :    #
+        )                      # OR
+        |                          #   A v4 address with NO leading zeros
+        (?:25[0-4]|2[0-4]\d|1\d\d|[1-9]?\d)
+        (?: \.
+        (?:25[0-4]|2[0-4]\d|1\d\d|[1-9]?\d)
+        ){3}
+        )
+        \s*                         # Trailing whitespace
+        $
+        """, re.VERBOSE | re.IGNORECASE | re.DOTALL)
+    return pattern.match(ip) is not None
+
+lines = {}
+today = datetime.date.today()
+
+actualTimeStamp = time.time()
+#actualTimeStamp = 1468317600
+beg_timestamp = actualTimeStamp - 1 * 60 * 60
+#beg_timestamp = 1468314000
+#actualTimeStamp = 1457481600
+#beg_timestamp = 1457395200
+number_of_lines = 1000
+
+idx = 0
+actualYear = datetime.datetime.fromtimestamp(int(beg_timestamp)).strftime('%Y')
+actualMonth = int(datetime.datetime.fromtimestamp(int(beg_timestamp)).strftime('%m'))
+
+db = MySQLdb.connect(host = DB_configuration.host, user = DB_configuration.user, passwd = DB_configuration.passwd, db="MergedData") # name of the data base
+
+cur = db.cursor()
+curMail = db.cursor()
+
+query = "select RouteCollector from AllRouteCollectors where Continent = 'AF' and TypeRC = 'RV';"
+cur.execute(query)
+route_all_collectors = cur.fetchall()
+
+# Create a new bgpstream instance and a reusable bgprecord instance
+
+for aux in route_all_collectors:
+    collector = aux[0]
+    print collector
+    sql_command = """select TypeRCid from AllRouteCollectors where RouteCollector = %s;"""
+    curMail.execute(sql_command, str(collector))
+    RouteCollectorID = curMail.fetchall()[0][0]
+        
+    route_collector = collector
+    timestamp1 = int(beg_timestamp)
+    timestamp2 = int(actualTimeStamp)
+    TypeRC = 'RV'
+    TypeRCid = RouteCollectorID
+
+    stream = BGPStream()
+    rec = BGPRecord()
+    # Consider RIPE RRC 10 only
+    stream.add_filter('collector',route_collector)
+
+    # Consider this time interval:
+    # Sat Aug  1 08:20:11 UTC 2015
+    stream.add_interval_filter(timestamp1,timestamp2)
+
+    # Start the stream
+    stream.start()
+    # Get next record
+    while(stream.get_next_record(rec)):
+        # Print the record information only if it is not a valid record
+        if rec.status != "valid":
+            print rec.project, rec.collector, rec.type, rec.time, rec.status
+        else:
+            elem = rec.get_next_elem()
+            while(elem):
+
+                # Print record and elem information
+                # print rec.project, rec.collector, rec.type, rec.time, rec.status,
+                Timestamp = rec.time
+                Date = datetime.datetime.fromtimestamp(rec.time)
+                Year = str(Date.year)
+                Month = str(Date.month)
+                Day = str(Date.day)
+
+                NextAS =  elem.peer_asn
+
+                if 'next-hop' in elem.fields.keys():
+                    NextHop = elem.fields['next-hop']
+                else:
+                    NextHop = 'None'
+
+                if 'prefix' in elem.fields.keys():
+                    Network = elem.fields['prefix']
+                else:
+                    Network = 'None'
+
+                if 'as-path' in elem.fields.keys():
+                    ASPath = elem.fields['as-path']
+                    ASPathLength = str(len(elem.fields['as-path'].split(" ")))
+                    OriginAS = (elem.fields['as-path'].split(" "))[int(ASPathLength)-1]
+                    #print OriginAS
+                else:
+                    ASPath = 'None'
+                    ASPathLength = 'None'
+                    OriginAS = 'None'
+                
+                if is_valid_v4(NextHop):
+                    IPversion = 'v4'
+                elif is_valid_v6(NextHop):
+                    IPversion = 'v6'
+                else:
+                    IPversion = 'None'
+
+                #print 'We can store it in the DB'
+                sql_command = """ INSERT INTO Data__"""+Year+"""_"""+Month+""" (TypeRC, TypeRCid, Date, Timestamp, Year, Month, Day, RouteCollector, OriginAS, NextAS, NextHop, Network, ASPath, ASPathLength, IP_version) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);"""
+                cur.execute(sql_command, (TypeRC, TypeRCid, Date, Timestamp, Date.year, Date.month, Date.day, route_collector, OriginAS, NextAS, NextHop, Network, ASPath, ASPathLength, IPversion))
+                idx += 1
+                if idx%number_of_lines == 0 and idx > 0:
+                    db.commit()
+      
+                #print 'Tipo de elemento:',elem.type 
+                #print 'Direccion:', elem.peer_address 
+                #print 'Campos:', elem.fields
+                elem = rec.get_next_elem()
+    
+    lines[collector] = idx
+    print idx
+    idx = 0
+    db.commit()
+
+today = datetime.date.today()
+sql_command = 'SELECT table_schema "Database Name", SUM( data_length + index_length)/1024/1024 "Database Size (MB)" FROM information_schema.TABLES where table_schema = "MergedData";'
+cur.execute(sql_command)
+size_of_DB = cur.fetchall()[0][1]
+beg_date = datetime.datetime.fromtimestamp(int(beg_timestamp)).strftime('%Y-%m-%d %H:%M:%S')
+actual_date = datetime.datetime.fromtimestamp(int(actualTimeStamp)).strftime('%Y-%m-%d %H:%M:%S')
+command = 'python costas_mail.py "roderick.fanou@gmail.com" "RouteViews download on '+str(today)+'" "We have downloaded RouteViews data from '+str(beg_date)+' ('+str(beg_timestamp)+') to '+str(actual_date)+' ('+str(actualTimeStamp)+')\nWe have collected '+str(lines['route-views.kixp'])+' lines for KIXP collector\nWe have collected lines for JINX collector\nThe size of the DB is '+str(size_of_DB)+' MB"'
+os.system(command)
---
+++ ComputationVM/Step3_DownloadPCHandRVdata/pch_download_daily.py
@@ -0,0 +1,127 @@
+#!/usr/bin/python
+import sys, os, csv, glob
+import time
+import MySQLdb
+import DB_configuration
+import datetime
+from datetime import date
+
+outfile = open('Debug_time.txt', 'a')
+outfile.write('The script has started at '+str(time.strftime("%d/%m/%Y %H:%M:%S"))+'.\n')
+
+#Correctly downloaded = [2003, 2005, 2006, 2007, 2008, 2009, 2013]
+#needed = 2010, 2011, 2012, 2014, 2015
+yearList = [str(date.today().year)]
+#monthList = ["01"]
+monthList = [str(datetime.datetime.now().strftime("%m"))]
+current_day = datetime.datetime.now().strftime("%d")
+#current_day = '14'
+Dictionary_locations = []
+Dictionary_files = {}
+Full_Dictionary = {}
+path = "/home/roderick/PCH_download"
+
+print "Start"
+
+#Select only the African RouteCollector
+db = MySQLdb.connect(host = DB_configuration.host, user = DB_configuration.user, passwd = DB_configuration.passwd, db="MergedData")
+cur = db.cursor()
+sql_command = """select RouteCollector from AllRouteCollectors where Continent = 'AF';"""
+cur.execute(sql_command)
+List = cur.fetchall() 
+
+
+RouteCollector_african  = []
+for rc in List :
+	RouteCollector_african.append(str(rc[0]).strip())
+
+print RouteCollector_african
+
+for elem in yearList:
+    for month in monthList:
+        Full_Dictionary[str(elem)] = {}
+        os.chdir(path)
+        command = "rm index.html"
+        os.system(command)
+        command = "wget https://www.pch.net/resources/Routing_Data/IPv4_daily_snapshots/" +str(elem)+"/"+str(month)+"/" 
+        print "\n Executing command =", command
+        os.system(command) 
+
+        find_file = "index.html"
+        print "\n find_file =", find_file
+
+        #<a href="/resources/data.php?dir=/2003//route-views.oregon-ix.net"><strong>route-views.oregon-ix.net</strong></a>
+        with open (find_file, "r") as index_file:
+            print "I access to file"
+            for line in index_file:
+                line = line.strip()
+                try:
+                    first = "<strong>"
+                    last = "</strong>"
+                    start = line.index(first) + len(first)
+                    end = line.index(last)
+                    ## Extracting the url of a "location folder"
+                    url_to_folder = line[start:end]
+                    ##Extracting the list of locations.
+                    folder_only = url_to_folder
+                    if 1:
+                        Dictionary_locations.append(folder_only)
+                except:
+                    pass
+
+        command = "mkdir "+str(elem)
+        os.system(command)
+        command = "mkdir "+str(elem)+'/'+str(month)
+        os.system(command) 
+        print "Creating folder: ", str(month)
+        os.chdir(path+"/"+str(elem)+"/"+str(month))
+
+
+        ##Download the data files of the locations
+        for folder_only in Dictionary_locations:
+            print folder_only
+            if folder_only in RouteCollector_african:
+                command = "rm index.html"
+                os.system(command)
+                command = "mkdir " +str(folder_only)
+                os.system(command)
+                print 'AQUI',folder_only
+                command = "wget https://www.pch.net/resources/Routing_Data/IPv4_daily_snapshots/"+str(elem)+"/"+str(month)+"/"+str(folder_only)+"/"
+                print "\n I launch the command: ", command
+                os.system(command) 
+                print 'AQUI NO',folder_only
+                os.chdir(path+"/"+str(elem)+"/"+str(month))
+                find_packets = "index.html"
+                Full_Dictionary[str(elem)][folder_only] = []
+
+                with open (find_packets, 'r') as index_packets:
+                    for line2 in index_packets:
+                        line2 = line2.strip()
+                        try:
+                            first2 = '<td data-sort-value="'
+                            last2 = '"><i class="fa fa-archive fa-fw">'
+                            start2 = line2.index(first2) + len(first2)
+                            end2 = line2.index(last2)
+                            url_to_file = line2[start2:end2] 
+                            ## Extracting the name of the data file
+                            file_only = url_to_file
+                            url_to_file = folder_only+"/"+file_only
+                            os.chdir(path+"/"+str(elem)+"/"+str(month)+"/"+str(folder_only))
+                            dia_de_descarga = '.'+current_day+'.gz'
+                            if (dia_de_descarga in url_to_file):
+                                command = " wget https://www.pch.net/resources/Routing_Data/IPv4_daily_snapshots/" + str(elem) +"/"+str(month)+"/"+url_to_file
+                                os.system(command) 
+                                print 'url command= ', command
+                                print 'url_to_file', url_to_file
+                                print 'file_only', file_only
+                                print 'folder_only', folder_only
+                                time.sleep(10)
+                        except:
+                            pass
+
+                        os.chdir(path+"/"+str(elem)+"/"+str(month))  
+os.chdir(path)                        
+command = 'python daily_victor_roderick_parsing_vtest.py'
+os.system(command)
+outfile = open('Debug_time.txt', 'a')
+outfile.write('The script has ended at '+str(time.strftime("%d/%m/%Y %H:%M:%S"))+'.\n')
